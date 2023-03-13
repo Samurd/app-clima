02121726e7d9a2bdcc84d214a656b9567c4ac36c
Samuel Viloria <samuelviloriadd@gmail.com>
2023-03-12 19:11:25 -0500

    Arreglo de imagenes, no cargaban

--- app.js
+++ app.js
@@ -37,23 +37,23 @@ search.addEventListener("click",() => {
 
         switch (json.weather[0].main) {
             case 'Clear':
-                image.src = '/images/clear.png';
+                image.src = './images/clear.png';
                 break;
 
             case 'Rain':
-                image.src = '/images/rain.png';
+                image.src = './images/rain.png';
                 break;
 
             case 'Snow':
-                image.src = '/images/snow.png';
+                image.src = './images/snow.png';
                 break;
 
             case 'Clouds':
-                image.src = '/images/clouds.png';
+                image.src = './images/clouds.png';
                 break;
 
             case 'Haze':
-                image.src = '/images/haze.png';
+                image.src = './images/haze.png';
                 break;
             default:
                 image.src = '';
